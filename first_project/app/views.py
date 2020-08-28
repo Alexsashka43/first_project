65a3136f2003367f6573f93a8f7fa96c9dc36b36
alexlit92 <alexlit92@mail.ru>
2020-08-28 23:24:38 +0300

    first commit

---
+++ first_project/app/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+import os
+
+def home_view(request):
+    template_name = 'app/home.html'
+
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = None
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    a = os.listdir('..')
+    string = ''
+    for items in a:
+        string += items + ', '
+    raise HttpResponse(string)
